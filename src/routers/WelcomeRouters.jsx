fd8887ea5c178857de18f127afdc461534ab2f07
Amaury Delille <amaurydelille92500@gmail.com>
2024-03-24 20:21:19 +0700

    fix

--- src/routers/WelcomeRouters.jsx
+++ src/routers/WelcomeRouters.jsx
@@ -12,6 +12,7 @@ import Admin from "../page/Admin/Admin";
 import Security from "../page/Security/Security";
 import Notification from "../page/Notification/Notification";
 import Privacyanddata from "../page/Privacyanddata/Privacyanddata";
+
 const welcomeRouterData = [
   { path: "findtalent", component: <FindTalent /> },
   { path: "ourhub", component: <OurHub /> },
@@ -23,8 +24,6 @@ const welcomeRouterData = [
   { path: "security", component: <Security /> },
   { path: "notification", component: <Notification /> },
   { path: "privacyanddata", component: <Privacyanddata /> },
-
-
 ];
 
 const DefaultComponent = () => {
