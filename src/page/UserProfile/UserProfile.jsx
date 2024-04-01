7474b7168b40f27e1f91980f73da2a1c5368ad71
Amaury Delille <amaurydelille92500@gmail.com>
2024-04-01 15:38:37 +0700

    fixed subscribe

--- src/page/UserProfile/UserProfile.jsx
+++ src/page/UserProfile/UserProfile.jsx
@@ -20,19 +20,19 @@ import ArtworkCard from "../../components/ArtWorkCard.jsx";
 import Cookies from "js-cookie";
 import {UserAddOutlined} from "@ant-design/icons";
 import './UserProfile.css';
+import { Spin } from 'antd';
+
 const UserProfile = () => {
-    
-  const { id } = useParams(); // Extract id from URL
+  const { id } = useParams();
   const [im, setim] = useState(null);
   const [pim, setpim] = useState(null);
   const [userArtworks, setUserArtworks] = useState([]);
-  const [follow, setFollow] = useState('Follow')
-
+  const [follow, setFollow] = useState('');
   const [user, setUser] = useState(null);
   const [followers, setFollowers] = useState([]);
-  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [sessionCookie, setSessionCookie] = useState("");
+  const [sessionCookie, setSessionCookie] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const cookieValue = Cookies.get("sessionCookie");
@@ -47,7 +47,7 @@ const UserProfile = () => {
           .post(`http://localhost:5000/api/users/${sessionCookie}/follow/${creatorId}`)
           .then(() => {
             setFollow('Unfollow');
-            setFollowers(prevFollowers => prevFollowers + 1);
+            fetchFollowers();
             message.success('Creator followed successfully');
           })
           .catch((error) => {
@@ -58,7 +58,7 @@ const UserProfile = () => {
           .delete(`http://localhost:5000/api/users/${sessionCookie}/follow/${creatorId}`)
           .then(() => {
             setFollow('Follow');
-            setFollowers(prevFollowers => prevFollowers - 1);
+            fetchFollowers()
             message.success('Creator unfollowed successfully');
           })
           .catch((error) => {
@@ -67,71 +67,62 @@ const UserProfile = () => {
     }
   };
 
-
-
   const fetchFollow = (userId, creatorId) => {
     axios
-        .get(`http://localhost:5000/api/users/${sessionCookie}/follow/${creatorId}`)
+        .get(`http://localhost:5000/api/users/${userId}/follow/${creatorId}`)
         .then((res) => {
-          if (res.data.follow === true)
+          if (res.data.follow === true) {
             setFollow('Unfollow');
-          else
+          }
+          else {
             setFollow('Follow');
+          }
         })
+        .finally(()=>{setLoading(false);})
+  };
+
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/users/${id}`);
+      setUser(response.data.user);
+      setLoading(false);
+    } catch (error) {
+      setError('Error fetching user profile');
+      setLoading(false);
+    }
+  };
+
+  const fetchFollowers = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/users/${id}/followers`);
+      setFollowers(response.data.followers);
+
+    } catch (error) {
+      setError('Error fetching user followers');
+    }
+  };
+  const fetchUserPosts = (userId) => {
+    axios
+        .get(`http://localhost:5000/api/users/${userId}/artworks`)
+        .then((response) => {
+          setUserArtworks(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching user posts:", error);
+        });
   };
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/api/users/${id}`);
-        setUser(response.data.user);
-        setLoading(false);
-      } catch (error) {
-        setError('Error fetching user profile');
-        setLoading(false);
-      }
-    };
-
-    const fetchFollowers = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/api/users/${id}/followers`);
-        setFollowers(response.data.followers);
-        console.log(response.data.followers);
-      } catch (error) {
-        setError('Error fetching user followers');
-      }
-    };
-    const fetchUserPosts = (userId) => {
-        axios
-          .get(`http://localhost:5000/api/users/${userId}/artworks`)
-          .then((response) => {
-            setUserArtworks(response.data);
-          })
-          .catch((error) => {
-            console.error("Error fetching user posts:", error);
-          });
-      };
+    fetchFollow(sessionCookie, id);
+    fetchFollowers();
     fetchUserPosts(id);
     fetchUser();
-    fetchFollowers();
-    fetchFollow(sessionCookie, id);
+    console.log(follow)
   }, [id]);
 
-  const renderImages = () => {
-    const handleArtworkClick = (title, description, image) => {
-      // Handle artwork click logic
-    };
-
-    return (
-      <div className="artworks">
-        <div className="artworksContainer">
-          {userArtworks.map((artwork, index) => (
-              <ArtworkCard key={index} artwork={artwork} />
-          ))}
-        </div>
-      </div>
-    );
-  };
+  useEffect(() => {
+    fetchFollow(sessionCookie, id);
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -144,57 +135,63 @@ const UserProfile = () => {
   }
 
   return (
-    <div>
-      {/* Profile header */}
-      <div className="profile-img" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: "100%", height: "400px" }}>
-        <div className="fileupload">
-          <img src={im ? im : prf} />
-        </div>
-        <Typography.Title
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontWeight: "bold",
-            fontSize: "20px",
-          }}
-        >
-          {user[0].name}
-        </Typography.Title>
-        <Typography.Text
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontSize: "15px",
-          }}
-        >
-          {followers} followers
-        </Typography.Text>
-        <Button className='button' onClick={() => handleFollowCreator(id)}><UserAddOutlined />{follow}</Button>
+      <div>
+        {/* Profile header */}
+        <div className="profile-img" style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', width: "100%", height: "400px" }}>
+          <div className="fileupload">
+            <img src={im ? im : prf} />
+          </div>
+          <Typography.Title
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                fontWeight: "bold",
+                fontSize: "20px",
+              }}
+          >
+            {user[0].name}
+          </Typography.Title>
+          <Typography.Text
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                fontSize: "15px",
+              }}
+          >
+            {followers} followers
+          </Typography.Text>
+          {/* loading ?
+            (
+                <Spin style={{ margin: 10 }}/>
+            ) : (
+                <button className='buttonFollow' onClick={() => handleFollowCreator(id)}><UserAddOutlined style={{ marginRight: 5 }}/>{follow}</button>
+            )*/}
+          <button className='buttonFollow' onClick={() => handleFollowCreator(id)}><UserAddOutlined style={{ marginRight: 5 }}/>{follow}</button>
 
-      </div>
-
-      {/* Artworks section */}
-      <div className="secondSection">
-        {/* Buttons container */}
-        <div
-          className="buttonsContainer"
-          style={{ display: "flex", justifyContent: "center" }}
-        >
-          {/* Add buttons if needed */}
         </div>
-        
-        {/* Render user artworks */}
-        <div className="artworks">
-          <div className="artworksContainer">
-            {userArtworks?.map((artwork, index) => (
-                <ArtworkCard key={index} artworkData={artwork} />
-            ))}
+
+        {/* Artworks section */}
+        <div className="secondSection">
+          {/* Buttons container */}
+          <div
+              className="buttonsContainer"
+              style={{ display: "flex", justifyContent: "center" }}
+          >
+            {/* Add buttons if needed */}
+          </div>
+
+          {/* Render user artworks */}
+          <div className="artworks">
+            <div className="artworksContainer">
+              {userArtworks?.map((artwork, index) => (
+                  <ArtworkCard key={index} artworkData={artwork} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
